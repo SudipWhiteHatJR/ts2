ffd449f42fac29ea8eb8db20af7d7ee6e44aa91a
SudipWhiteHatJR <74854918+SudipWhiteHatJR@users.noreply.github.com>
2021-02-07 12:13:18 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,37 @@
+class Chain{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.2,
+            length: 10,
+        }
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+    attach(body){
+        this.sling.bodyA = body;
+    }
+    display(){
+       
+            stroke("black")
+            strokeWeight(4);
+            if(this.sling.bodyA){
+                var pointA = this.sling.bodyA.position;
+                var pointB = this.pointB;
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+           
+            
+        }
+    }
+
+    
+    }
+    
+
+
